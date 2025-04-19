9aef8d9b8e5bc176d579e8c3dd02f831816b0348
HCarlb <47041402+HCarlb@users.noreply.github.com>
2025-04-20 00:57:49 +0200

    moved ribbon stuff into own controller

--- ExcelAddIn/Connect.cs
+++ ExcelAddIn/Connect.cs
@@ -20,7 +20,7 @@ public class Connect : IDTExtensibility2 , IRibbonExtensibility, ICustomTaskPane
     private readonly string _ribbonName = "Ribbon.xml";
     private readonly string _ribbonPath = "Ribbons";
     private XlApplication? _xlApp;
-
+    private RibbonController? _ribbonController;
     /* 
      * ################################################################################################################################
      * 
@@ -35,9 +35,6 @@ public class Connect : IDTExtensibility2 , IRibbonExtensibility, ICustomTaskPane
     public void OnBeginShutdown(ref Array custom)
     {
         Log.Information("Add-in is being unloaded.");
-        // This method is called when the add-in is being unloaded.
-        // You can perform any necessary cleanup here.
-
         Log.CloseAndFlush();
     }
 
@@ -47,84 +44,50 @@ public class Connect : IDTExtensibility2 , IRibbonExtensibility, ICustomTaskPane
         Log.Logger = Configuration.ConfigureLogger();
         Log.Information("Add-in is being loaded.");
 
-        // This method is called when the add-in is loaded.
-        // You can perform any initialization here.
-
+        // Initialize the excel application object
         _xlApp = application as XlApplication;
-    }
 
-    public void OnDisconnection(ext_DisconnectMode removeMode, ref Array custom)
-    {
-        // This method is called when the add-in is unloaded.
-        // You can perform any necessary cleanup here.
-        Log.Debug("Add-in is being unloaded.");
-    }
-
-    public void OnStartupComplete(ref Array custom)
-    {
-        // This method is called when the add-in has finished loading.
-        // You can perform any final initialization here.
         if (_xlApp == null)
         {
             Log.Error("Application object is not initialized.");
             return;
         }
 
+        // RibbonController is used to manage the custom ribbon UI
+        _ribbonController = new RibbonController(_xlApp);
+    }
+
+    public void OnDisconnection(ext_DisconnectMode removeMode, ref Array custom)
+    {
+        Log.Debug("Add-in is being unloaded.");
+
+        // Clean up
+        _ribbonController?.Dispose();
+        _ribbonController = null;
+        _xlApp = null;
+    }
+
+    public void OnStartupComplete(ref Array custom)
+    {
         Log.Debug("Greeting the user with Hello World!");
-        _xlApp.ActiveSheet.Cells[1, 1].Value = "Hello, World!";
+        _xlApp!.ActiveSheet.Cells[1, 1].Value = "Hello, World!";
         
         Log.Information("Add-in has finished loading.");
     }
 
     public void OnAddInsUpdate(ref Array custom)
     {
-        // This method is called when the add-in is updated.
-        // You can perform any necessary updates here.
         Log.Debug("Add-in has been updated.");
-    }
-
-    public string GetCustomUI(string RibbonID)
-    {
-        try
-        {
-            return RibbonExtensions.GetRibbonXML(_ribbonName, _ribbonPath);
-        }
-        catch (Exception ex)
-        {
-            Log.Error("Error loading ribbon XML: {0}", ex.Message);
-            return string.Empty;
-        }
     }
 
     public void CTPFactoryAvailable(ICTPFactory CTPFactoryInst)
     {
-        // This method is called when the CTP factory is available.
-        // CPT factory is used to create custom task panes.
         Log.Debug("CTPFactory available.");
     }
 
-    public void OnRibbonLoaded(IRibbonUI ribbonUI)
-    {
-        // This method is called when the ribbon is loaded from onLoad="OnRibbonLoad" in the xml ribbon.
-        // You can perform any necessary initialization here.
-        Log.Debug("Ribbon loaded.");
-
-        _ = ribbonUI;   // To discard the warning about unused variable.
-    }
-    
-    public void OnButtonClick(IRibbonControl control)
-    {   
-        // This method is called when the button is clicked.
-        // You can perform any necessary actions here.
-        Log.Debug("Button clicked: {0}", control.Id);
-        if (_xlApp is null)
-        {
-            Log.Error("Application object is not initialized.");
-            return;
-        }
-
-        _xlApp.ActiveSheet.Cells[1, 2].Value = $"Button [{control.Id}] was Clicked";
-
-        MessageBox.Show($"Button [{control.Id}] was Clicked", "Button Clicked", MessageBoxButtons.OK, MessageBoxIcon.Information);
-    }
+    #region RibbonController
+    public string GetCustomUI(string RibbonID) => _ribbonController?.GetCustomUI(RibbonID) ?? string.Empty;
+    public void OnRibbonLoaded(IRibbonUI ribbonUI) => _ribbonController?.OnLoaded(ribbonUI);
+    public void OnAction(IRibbonControl control) => _ribbonController?.OnAction(control);
+    #endregion RibbonController
 }
---
+++ ExcelAddIn/RibbonController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Office.Core;
+using XlApplication = Microsoft.Office.Interop.Excel.Application;
+
+namespace HcExcelAddIn;
+
+public sealed class RibbonController(XlApplication xlApp) : IRibbonExtensibility, IDisposable
+{
+    private readonly string _ribbonName = "Ribbon.xml";
+    private readonly string _ribbonPath = "Ribbons";
+
+    private readonly XlApplication _xlApp = xlApp;
+    private IRibbonUI? _ribbon;
+    private bool _disposed = false;
+    private bool _loaded = false;
+
+    public void OnLoaded(IRibbonUI ribbon)
+    {
+        _ribbon = ribbon;
+        _loaded = true;
+        Log.Information("Ribbon loaded.");
+    }
+
+    public string GetCustomUI(string RibbonID) => RibbonExtensions.GetRibbonXML(_ribbonName, _ribbonPath);
+
+    public void OnAction(IRibbonControl control)
+    {
+        // Handle ribbon button click events here
+        switch (control.Id)
+        {
+            case "button1":
+                MessageBox.Show("Button 1 clicked!", "Ribbon Button Clicked", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                break;
+            default:
+                Log.Warning($"Unknown control ID: {control.Id}");
+                break;
+        }
+    }
+
+    public void Dispose()
+    {
+        if (!_disposed)
+        {
+            // Cleanup future resources or event handlers
+            _loaded = false;
+            _ribbon = null;
+            _disposed = true;
+            Log.Debug("RibbonController disposed.");
+        }
+    }
+}
