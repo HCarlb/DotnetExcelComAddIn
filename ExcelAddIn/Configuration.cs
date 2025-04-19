a6312494a47fdb57af5fb12f97312cf48e151431
HCarlb <47041402+HCarlb@users.noreply.github.com>
2025-04-19 18:09:47 +0200

    added windows forms.
    cleaned up logging code

---
+++ ExcelAddIn/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+namespace HcExcelAddIn;
+
+internal static class Configuration
+{
+    internal static ILogger ConfigureLogger()
+    {
+        var folder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? $"logs";
+        var logFilePath = Path.Combine(folder, "log-.txt");
+
+        var logger = new LoggerConfiguration()
+            .MinimumLevel.Debug() // Change to Information or Warning in production
+            .Enrich.FromLogContext()
+            .WriteTo.File(
+                path: logFilePath,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj} {Properties}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day,
+                restrictedToMinimumLevel: LogEventLevel.Debug,      // Change to Information or Warning in production
+                retainedFileCountLimit: 7 // Keep logs for last X days
+            )
+            .CreateLogger();
+
+        logger.Debug("Logger configured.");
+
+        return logger;
+    }
+}
